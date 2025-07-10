d8a0c19d66136c17003c7560d100c7f709c377a0
Natasha Aidomon <natasha.aidomon@gmail.com>
2025-07-10 09:34:04 +0100

    updated app.py

--- app.py
+++ app.py
@@ -4,10 +4,6 @@ import os
 
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # default to 5000 for local dev
-    app.run(host='0.0.0.0', port=port)
-
 conn = sqlite3.connect('todo.db', check_same_thread=False)
 cursor = conn.cursor()
 
